66cc7858b12026b300d7ad2adcecb8a6c7f73654
Eric LaMar <elamar@t-sciences.com>
2017-07-05 15:44:27 -0700

    Bug 168, part 2 - allow script to be run from anywhere, but put files in /opt/google/share/tutorials/fusion. Also, edit https://github.com/google/earthenterprise/wiki/Install-Fusion-or-Earth-Server; adding instructions on how to run 'download_tutorial.sh'

--- earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
+++ earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TMPFILE=/tmp/FusionTutorial-Full.tar.gz
+SCRIPTDIR=`dirname $0`
 
 if [ ! -d Imagery ]; then
 	wget http://data.opengee.org/FusionTutorial-Full.tar.gz -O $TMPFILE
-	tar -xvzf $TMPFILE -C .
+	tar -xvzf $TMPFILE -C $SCRIPTDIR
 	rm -f $TMPFILE
 fi
