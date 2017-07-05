110ff48c5fa78a8c2afe680a31af2bf97d73afcc
Eric LaMar <elamar@t-sciences.com>
2017-07-05 14:51:17 -0700

    Bug 168, part 2 - change over to local variable

--- earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
+++ earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+TMPFILE=/tmp/FusionTutorial-Full.tar.gz
+
 if [ ! -d Imagery ]; then
-	wget http://data.opengee.org/FusionTutorial-Full.tar.gz -O /tmp/FusionTutorial-Full.tar.gz
-	tar -xvzf /tmp/FusionTutorial-Full.tar.gz -C .
-	rm -f /tmp/FusionTutorial-Full.tar.gz
+	wget http://data.opengee.org/FusionTutorial-Full.tar.gz -O $TMPFILE
+	tar -xvzf $TMPFILE -C .
+	rm -f $TMPFILE
 fi
