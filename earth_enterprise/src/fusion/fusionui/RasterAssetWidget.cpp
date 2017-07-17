4f6cc341a199b7a7dd9418413574afbad0096c8f
tst-awankhede <awankhede@t-sciences.com>
2017-07-17 14:16:41 -0700

    Once failed to save a new resource, the resource cannot be saved again

--- earth_enterprise/src/fusion/fusionui/RasterAssetWidget.cpp
+++ earth_enterprise/src/fusion/fusionui/RasterAssetWidget.cpp
@@ -660,8 +660,8 @@ void RasterAssetWidget::AssembleEditRequest(
                           khErrnoException::errorString(errno));
       case SourceConfig::NonVolume:
         throw khException(filename + tr(" doesn't reside on a known volume.\n") +
-                         " You can move your asset files to a known volume, or create a new volume that contains their current location." +
-                         " You can list known volumes and create new ones from the command line with geconfigureassetroot.");
+                         " You can move your asset files to a known volume, or create a new volume\n" +
+			 " that contains their current location using geconfigureassetroot command.");
     }
   }
 
