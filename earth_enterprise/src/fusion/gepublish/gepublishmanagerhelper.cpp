bb851d148efe5a13ff8357eb1fa0fb740318bbd2
Chris Camp <ccamp@t-sciences.com>
2017-07-24 16:01:31 -0500

    Enables Build on CentOS 6 (#101) (#364)
    * Getting GEE to compile and run under CentOS 6
    * This currently only works with CentOS 6 and will need some updates in order to work on CentOS 6 as well as the other OS versions supported

--- earth_enterprise/src/fusion/gepublish/gepublishmanagerhelper.cpp
+++ earth_enterprise/src/fusion/gepublish/gepublishmanagerhelper.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <stddef.h>
+
 #include "fusion/gepublish/gepublishmanagerhelper.h"
 
 #include "fusion/dbmanifest/dbmanifest.h"
