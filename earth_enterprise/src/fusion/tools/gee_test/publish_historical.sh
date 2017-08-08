6eda4b74601c628d0244623d5423a8bfd7286ad2
Eric LaMar <elamar@t-sciences.com>
2017-08-07 18:16:32 -0700

    I-386 - update copyright text as per request.

--- earth_enterprise/src/fusion/tools/gee_test/publish_historical.sh
+++ earth_enterprise/src/fusion/tools/gee_test/publish_historical.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 #
-# Copyright 2017 Open GEE Authors
+# Copyright 2017 Open GEE Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
