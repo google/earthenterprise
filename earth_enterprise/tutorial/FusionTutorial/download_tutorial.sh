27f7dd61e1abed65e2b40c13d0df852416c1b0f1
Eric LaMar <elamar@t-sciences.com>
2017-08-16 15:52:24 -0700

    I362 run download_tutorial.sh requiring root

--- earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
+++ earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
@@ -3,6 +3,11 @@
 TMPFILE="/tmp/FusionTutorial-Full.tar.gz"
 SCRIPTDIR=`dirname "$0"`
 
+if [ "$EUID" != "0" ] ; then
+    echo `basename $0` "must be run as root"
+    exit 1;
+fi
+
 if [ ! -d "$SCRIPTDIR/Imagery" ]; then
 	wget "http://data.opengee.org/FusionTutorial-Full.tar.gz" -O "$TMPFILE"
 	tar -xvzf "$TMPFILE" -C "$SCRIPTDIR"
