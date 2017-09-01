e6df483c101ab77da4f228bf754eca2655ead4a1
tst-awankhede <awankhede@t-sciences.com>
2017-09-01 11:26:36 -0700

    #354 - Support building on CentOS6 with Python2.6

--- earth_enterprise/src/server/wsgi/serve/publish/publish_manager_helper.py
+++ earth_enterprise/src/server/wsgi/serve/publish/publish_manager_helper.py
@@ -1417,6 +1417,7 @@ class PublishManagerHelper(stream_manager.StreamManager):
       (scheme://host[:port]) -  {vh_name: vh_base_url}
     """
     vh_list = self.QueryVhList()
+    vhname_to_baseurl_dct = []
     for (vh_name, vh_url, vh_ssl) in vh_list:
         vhname_to_baseurl_dct[vh_name] = self.GetVhBaseUrl(vh_url, vh_ssl)
 
