7187f892848a7751ad865466f4816b05d4b90a9c
Pavel Penev <ppenev@t-sciences.com>
2017-09-05 23:16:54 -0400

    I389: Default UID and GID to 1000 when creating X users in Docker container.
    * The defaults on RHEL 6 without specifying a UID and a GID are IDs under 1000
      which don't work on most host systems.

--- earth_enterprise/docker/image-definition/scripts/create_x_user.sh
+++ earth_enterprise/docker/image-definition/scripts/create_x_user.sh
@@ -5,23 +5,31 @@
 
 SELF_NAME=$(basename "$0")
 
-if [ -n "$X_USER_ID" ]; then
-    UID_ARGUMENT="--uid $X_USER_ID"
-else
-    UID_ARGUMENT=""
+if [ -z "$X_USER_ID" ]; then
+    # Default to user ID 1000 for an X user.  Older versions of `useradd`
+    # default to under-1000 UIDs (e.g. 500), which fail for most host systems.
+    X_USER_ID=1000
 fi
 
-if [ -n "$X_USER_GID" ]; then
-    GID_ARGUMENT="--gid $X_USER_GID"
-else
-    GID_ARGUMENT=""
+UID_ARGUMENT="--uid $X_USER_ID"
+
+
+if [ -z "$X_USER_GID" ]; then
+    # Default to group ID 1000 for an X user.  Older versions of `useradd`
+    # default to under-1000 GIDs (e.g. 500), which fail for most host systems.
+    X_USER_GID=1000
 fi
 
-if [ -n "$UID_ARGUMENT" ]; then
-    if [ getent passwd "$X_USER_ID" ]; then
-        echo "$SELF_NAME: X user with ID $X_USER_ID already exists. Quitting."
-        exit 0
-    fi
+GID_ARGUMENT="--gid $X_USER_GID"
+
+# Add a group with the given group ID, if such doesn't exist:
+if ! getent group "$X_USER_GID"; then
+    groupadd --gid "$X_USER_GID" x_users
+fi
+
+if getent passwd "$X_USER_ID"; then
+    echo "$SELF_NAME: X user with ID $X_USER_ID already exists. Quitting."
+    exit 0
 fi
 
 useradd --create-home $UID_ARGUMENT $GID_ARGUMENT x_user
