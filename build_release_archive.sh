e13077d15cc212d87d4e27493ed83fb23a5f34d4
Lee Savoie <lsavoie@t-sciences.com>
2017-09-12 10:38:16 -0700

    Clean up based on review comments

--- build_release_archive.sh
+++ build_release_archive.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 # This script creates archive files that can be distributed as a release. The
-# automatically generated files on the github releases page do not include the
-# files stored in git lfs, so this process is required to create working
-# archives that we can publish on the github releases page.
+# automatically generated files on the GitHub releases page do not include the
+# files stored in Git LFS, so this process is required to create working
+# archives that we can publish on the GitHub releases page.
 
 # This script places the generated archives in the current directory.  Because
 # this script downloads and packages a large repo it may take a long time to
 # run.
 
-# This script works under /tmp and it assumes that previous artifacts under /tmp
-# (such as previously downloaded repos or previously created archives) can be
-# safely deleted.
-
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <committish>"
-  echo "where <committish> is a commit, branch, or tag that defines the release"
-  echo "to be archived."
+  echo "Usage: $0 <branch>"
+  echo "where <branch> is a branch or tag that defines the release to be archived."
   exit 1
 fi
 
@@ -29,33 +24,32 @@ if [ -z "${SMUDGE_HOOK}" ]; then
 fi
 
 REPO_NAME="earthenterprise"
-CLONE_DIR="/tmp"
-
 TAR_FILE=${REPO_NAME}.tar.gz
 ZIP_FILE=${REPO_NAME}.zip
 RUN_DIR=`pwd`
 
+# Make a temporary directory
+CLONE_DIR=`mktemp -d`
+
 # Download the repository
-cd ${CLONE_DIR}
-rm -Rf ${REPO_NAME}
-git clone git@github.com:google/${REPO_NAME}.git || exit
-cd ${REPO_NAME} || exit
-git checkout ${1} || exit
+cd "${CLONE_DIR}"
+git clone -b "${1}" --depth 1 git@github.com:google/${REPO_NAME}.git || exit
 
 # Remove the git-related files from the repo
+cd "${REPO_NAME}"
 FILES=`find . -name ".git*"`
 for FILE in ${FILES}; do
-  rm -Rf ${FILE}
+  rm -Rf "${FILE}"
 done
 
 # Create the archives
-cd ${CLONE_DIR}
-rm -f ${TAR_FILE} ${ZIP_FILE}
-tar czf ${TAR_FILE} ${REPO_NAME}
-zip -rq ${ZIP_FILE} ${REPO_NAME}
+cd "${CLONE_DIR}"
+tar czf "${TAR_FILE}" "${REPO_NAME}"
+zip -rq "${ZIP_FILE}" "${REPO_NAME}"
 
 # Clean up
-mv ${TAR_FILE} ${RUN_DIR}
-mv ${ZIP_FILE} ${RUN_DIR}
-rm -Rf ${REPO_NAME}
+mv "${TAR_FILE}" "${RUN_DIR}"
+mv "${ZIP_FILE}" "${RUN_DIR}"
+cd
+rm -Rf "${CLONE_DIR}"
 
