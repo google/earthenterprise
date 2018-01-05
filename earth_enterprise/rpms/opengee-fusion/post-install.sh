57972d7ff134f9daf18a663bfc890368e170e1e8
David Sugar <dsugar@t-sciences.com>
2018-01-05 11:24:09 -0500

    new test for asset ownership

--- earth_enterprise/rpms/opengee-fusion/post-install.sh
+++ earth_enterprise/rpms/opengee-fusion/post-install.sh
@@ -153,7 +153,8 @@ install_or_upgrade_asset_root()
     else
         # upgrade asset root -- if this is a master
         if [ "$IS_SLAVE" = "false" ]; then
-            if [ "$NEW_INSTALL" = "true" ] ; then
+            OWNERSHIP=`find /gevol/assets -maxdepth 0 -printf "%g:%u"`
+            if [ "$OWNERSHIP" != "$GEGROUP:$GEFUSIONUSER" ] ; then
                 NOCHOWN=""
                 UPGRADE_MESSAGE="The upgrade will fix permissions for the asset root and source volume. This may take a while."
             else
