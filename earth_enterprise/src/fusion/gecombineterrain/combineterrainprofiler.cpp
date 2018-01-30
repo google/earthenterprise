7c6bd39e2b2ab06345c8171e6ab9298e7317fcae
Lee Savoie <lsavoie@t-sciences.com>
2018-01-30 07:56:23 -0700

    Issue 678: Add comments

--- earth_enterprise/src/fusion/gecombineterrain/combineterrainprofiler.cpp
+++ earth_enterprise/src/fusion/gecombineterrain/combineterrainprofiler.cpp
@@ -1,3 +1,17 @@
+// Copyright 2018 the Open GEE Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <iomanip>
 #include <sstream>
 #include <string>
@@ -18,6 +32,7 @@ CombineTerrainProfiler * const CombineTerrainProfiler::_instance =
 CombineTerrainProfiler * const BlockProfiler::terrainProf =
     CombineTerrainProfiler::instance();
 
+// Log a profiling message
 void CombineTerrainProfiler::log
     (TerrainEvent event, const string operation, const string object) {
   stringstream message;
--- earth_enterprise/src/fusion/gecombineterrain/combineterrainprofiler.h
+++ earth_enterprise/src/fusion/gecombineterrain/combineterrainprofiler.h
@@ -1,3 +1,17 @@
+// Copyright 2018 the Open GEE Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef COMBINETERRAINPROFILER_H
 #define COMBINETERRAINPROFILER_H
 
@@ -5,6 +19,12 @@
 
 #include "quadtreepath.h"
 
+/*
+ * Singleton class for profiling terrain combine events. The programmer can use
+ * this class to mark the beginning and end of events, and the timing of those
+ * events will be written out to a log file. This class is intended for
+ * performance debugging.
+ */
 class CombineTerrainProfiler
 {
   public:
@@ -29,6 +49,11 @@ class CombineTerrainProfiler
     double getTime() const;
 };
 
+/*
+ * A convenience class for profiling a block of code. Profiling begins when an
+ * instance of this class is created and ends when the instance goes out of
+ * scope.
+ */
 class BlockProfiler
 {
   public:
@@ -46,6 +71,9 @@ class BlockProfiler
     const QuadtreePath path;
 };
 
+// Programmers should use these functions to profile code rather than the
+// functions above. This makes it easy to use compile time flags to exclude
+// the profiling code.
 #define BEGIN_TERRAIN_PROF(op, path) { BlockProfiler prof(op, path)
 #define END_TERRAIN_PROF() }
 
