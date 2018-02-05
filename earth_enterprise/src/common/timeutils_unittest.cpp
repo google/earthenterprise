fba13f78def43fafb32553e9226d6f9ee281de9b
Lee Savoie <lsavoie@t-sciences.com>
2018-02-05 15:11:27 -0700

    Issue #678: Additional timeutils tests

--- earth_enterprise/src/common/timeutils_unittest.cpp
+++ earth_enterprise/src/common/timeutils_unittest.cpp
@@ -13,7 +13,10 @@
 // limitations under the License.
 
 #include <gtest/gtest.h>
+#include <iomanip>
+#include <sstream>
 #include <time.h>
+#include <unistd.h>
 
 #include "timeutils.h"
 
@@ -24,6 +27,18 @@ timespec makeTimespec(time_t sec, time_t nsec) {
   result.tv_sec = sec;
   result.tv_nsec = nsec;
   return result;
+}
+
+TEST(TimeUtilsTest, GetMonotonicTimeTest) {
+  timespec start = getMonotonicTime();
+  sleep(2);
+  timespec end = getMonotonicTime();
+  EXPECT_GE(end.tv_sec, start.tv_sec + 2);
+}
+
+TEST(TimeUtilsTest, TimespecToDoubleTest) {
+  timespec x = makeTimespec(123, 456789123);
+  EXPECT_EQ(timespecToDouble(x), (double)123.456789123);
 }
 
 void testTimespecDiff(time_t x_sec, time_t x_nsec,
@@ -64,6 +79,69 @@ TEST(TimeUtilsTest, TimespecDiff_SmallMinusLarge) {
   EXPECT_DEATH_IF_SUPPORTED(timespecDiff(makeTimespec(10, 0), makeTimespec(11, 0)), ".*");
 }
 #endif
+
+TEST(TimeUtilsTest, TimespecLT_Sec) {
+  timespec x = makeTimespec(1, 123);
+  timespec y = makeTimespec(2, 123);
+  EXPECT_TRUE(x < y);
+  EXPECT_FALSE(y < x);
+}
+
+TEST(TimeUtilsTest, TimespecLT_NSec) {
+  timespec x = makeTimespec(1, 123);
+  timespec y = makeTimespec(1, 124);
+  EXPECT_TRUE(x < y);
+  EXPECT_FALSE(y < x);
+}
+
+TEST(TimeUtilsTest, TimespecLT_Both) {
+  timespec x = makeTimespec(1, 123);
+  timespec y = makeTimespec(2, 124);
+  EXPECT_TRUE(x < y);
+  EXPECT_FALSE(y < x);
+}
+
+TEST(TimeUtilsTest, TimespecLT_Same) {
+  timespec x = makeTimespec(1, 123);
+  timespec y = makeTimespec(1, 123);
+  EXPECT_FALSE(x < y);
+  EXPECT_FALSE(y < x);
+}
+
+TEST(TimeUtilsTest, TimespecGE_Sec) {
+  timespec x = makeTimespec(2, 123);
+  timespec y = makeTimespec(1, 123);
+  EXPECT_TRUE(x >= y);
+  EXPECT_FALSE(y >= x);
+}
+
+TEST(TimeUtilsTest, TimespecGE_NSec) {
+  timespec x = makeTimespec(1, 124);
+  timespec y = makeTimespec(1, 123);
+  EXPECT_TRUE(x >= y);
+  EXPECT_FALSE(y >= x);
+}
+
+TEST(TimeUtilsTest, TimespecGE_Both) {
+  timespec x = makeTimespec(2, 124);
+  timespec y = makeTimespec(1, 123);
+  EXPECT_TRUE(x >= y);
+  EXPECT_FALSE(y >= x);
+}
+
+TEST(TimeUtilsTest, TimespecGE_Same) {
+  timespec x = makeTimespec(1, 123);
+  timespec y = makeTimespec(1, 123);
+  EXPECT_TRUE(x >= y);
+  EXPECT_TRUE(y >= x);
+}
+
+TEST(TimeUtilsTest, StreamInsertionTest) {
+  timespec x = makeTimespec(9, 87654321);
+  std::stringstream stream;
+  stream << std::setprecision(6) << x;
+  EXPECT_EQ(stream.str(), "9.08765");
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
