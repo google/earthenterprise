087889cd22bce87702c0119697a4df4ce1e41600
Chris Camp <ccamp@t-sciences.com>
2018-03-02 16:34:38 -0700

    Issue #720 RPM scriptlets should not ignore errors * Added '-e' flag to scriptlets to return errors that occur to RPM installer * Updated a few places that were causing errors incorrectly

--- earth_enterprise/rpms/opengee-fusion/post-install.sh
+++ earth_enterprise/rpms/opengee-fusion/post-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright 2017 The Open GEE Contributors
+# Copyright 2018 The Open GEE Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 # NOTE: requires xmllint from libxml2-utils
 
 set +x
+set -e
 
 #------------------------------------------------------------------------------
 # Definitions
@@ -191,8 +192,8 @@ final_assetroot_configuration()
         "$BASEINSTALLDIR_OPT/bin/geselectassetroot" --assetroot "$ASSET_ROOT"
 
         "$BASEINSTALLDIR_OPT/bin/geconfigureassetroot" --addvolume \
-            "opt:$BASEINSTALLDIR_OPT/share/tutorials" --noprompt --nochown
-        if [ $? -eq 255 ]; then
+            "opt:$BASEINSTALLDIR_OPT/share/tutorials" --noprompt --nochown || RET_VAL=$?
+        if [ "$RET_VAL" -eq "255" ]; then
             cat <<END
 The geconfigureassetroot utility has failed while attempting
 to add the volume 'opt:$BASEINSTALLDIR_OPT/share/tutorials'.
--- earth_enterprise/rpms/opengee-fusion/pre-install.sh
+++ earth_enterprise/rpms/opengee-fusion/pre-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright 2017 The Open GEE Contributors
+# Copyright 2018 The Open GEE Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
 # limitations under the License.
 
 set +x
+set -e
+
 NEW_INSTALL=false
 if [ "$1" = "1" ] ; then
     NEW_INSTALL=true
