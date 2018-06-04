65b91ac450ff4272b43f7a3d94ca099b71c83749
Vimal Raghubir <vraghubir0418@gmail.com>
2018-06-04 14:36:26 -0400

    Removed unused comment

--- earth_enterprise/src/fusion/fusionui/AssetProperties.cpp
+++ earth_enterprise/src/fusion/fusionui/AssetProperties.cpp
@@ -68,7 +68,6 @@ AssetProperties::AssetProperties( QWidget* parent, const gstAssetHandle &handle 
     : AssetPropertiesBase( parent, 0, false, Qt::WDestructiveClose ), assetHandle( handle )
 {
   versionsList->setSorting( 0, false );
-  //AcquisitionDate->setSeparator( "-" );
 
   Asset asset = handle->getAsset();
   nameLabel->setText( shortAssetName( handle->getName() ) );
