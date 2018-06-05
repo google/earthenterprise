d5873609359d7a0c02e7564d32a17a105991ba40
Rick Wildes <rwildes@t-sciences.com>
2018-06-05 16:00:24 -0400

    Update copyrights

--- earth_enterprise/src/server/wsgi/serve/stream_manager.py
+++ earth_enterprise/src/server/wsgi/serve/stream_manager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #
 # Copyright 2017 Google Inc.
+# Copyright 2018 Open GEE Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
