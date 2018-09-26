da8547b90ae462344f8960e796b3f3b497702683
Kirk Freiheit <38008517+tst-kfreiheit@users.noreply.github.com>
2018-09-26 13:38:35 -0400

    997 git based alpha builds (#1013)
    * Refactoring git-based versioning logic into separate python script
    
    * earth_enterprise/src/version.txt is now in the .gitignore; removing obsolete code and extraneous imports
    
    * Refining public/private interface to GetVersion
    
    * Using git tag instead of describe to get the tag with the highest version number that's reachable from HEAD.  The version number being built is one past that number.
    
    * Reverting to using git describe instead of tag for raw version string
    
    * Ensuring first alpha build will be patch number 0
    
    * Ignoring vscode directory globally
    
    * Using git tag command to determine baseline version tag for calculating current build version
    
    * Restoring hash to version components
    
    * Normalizing whitespace, adding func for _GetDateString to ensure consistency
    
    * Reordering functions for top-down readability
    
    * Moving GetRepository lower in file for readability
    
    * Refactoring: _VersionFromTagHistory() function extracted for readability and testability

---
+++ earth_enterprise/src/scons/getversion.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+import os
+import argparse
+import git
+from datetime import datetime
+
+def GetVersion(backupFile, label=''):
+    """As getLongVersion(), but only return the leading *.*.* value."""
+
+    raw = GetLongVersion(backupFile, label)
+    final = raw.split("-")[0]
+
+    return final
+
+
+def GetLongVersion(backupFile, label=''):
+    """Create a detailed version string based on the state of
+    the software, as it exists in the repository."""
+ 
+    if _CheckGitAvailable():
+        ret = _GitGeneratedLongVersion()
+
+  # Without git, must use the backup file to create a string.
+    else:
+        base = _ReadBackupVersionFile(backupFile)
+        ret = '-'.join([base, _GetDateString()])
+
+  # Append the label, if there is one.
+    if len(label):
+        ret = '.'.join([ret, label])
+
+    return ret
+
+
+def _GitGeneratedLongVersion():
+    """Take the raw information parsed by git, and use it to
+       generate an appropriate version string for GEE."""
+
+
+    # For tagged commits, return the tag itself
+    if _IsCurrentCommitTagged():
+        return _VersionForTaggedHead()
+    else:
+        return _VersionFromTagHistory()
+
+
+def _IsCurrentCommitTagged():
+    """True if the current commit is tagged, otherwise False"""
+    raw = _GetCommitRawDescription()
+    # If this condition hits, then we are currently on a tagged commit.
+    return (len(raw.split("-")) < 4)
+
+
+def _GetCommitRawDescription():
+    """Returns description of current commit"""
+    repo = _GetRepository()
+    raw = repo.git.describe('--tags', '--match', '[0-9]*\.[0-9]*\.[0-9]*\-*')
+    raw = raw.rstrip()
+    return raw
+
+
+def _VersionForTaggedHead():
+    """When we're on the tagged commit, the version string is
+    either the tag itself (when repo is clean), or the tag with
+    date appended (when repo has uncommitted changes)"""
+    raw = _GetCommitRawDescription()
+    if _CheckDirtyRepository():
+        # Append the date if the repo contains uncommitted files
+        return '.'.join([raw, _GetDateString()])
+    return raw
+
+
+def _VersionFromTagHistory():
+    """From the HEAD revision, this function finds the most recent
+    reachable version tag and returns a string representing the
+    version being built -- which is one version beyond the latest
+    found in the history."""
+    tag = _GetCommitVersionTag()
+
+    # Tear apart the information in the version string.
+    components = _ParseRawVersionString(tag)
+
+    # Determine how to update, since we are *not* on tagged commit.
+    if components['isFinal']:
+        components['patch'] = 0
+        components['patchType'] = "alpha"
+        components['revision'] = components['revision'] + 1
+    else:
+        components['patch'] = components['patch'] + 1
+    
+    # Rebuild.
+    base = '.'.join([str(components[x]) for x in ("major", "minor", "revision")])
+    patch = '.'.join([str(components["patch"]), components["patchType"], _GetDateString()])
+    if not _CheckDirtyRepository():
+        patch = '.'.join([patch, components['hash']])
+    
+    return '-'.join([base, patch])
+
+
+def _GetCommitVersionTag():
+    """Returns the highest tag that's reachable from the current HEAD of git repo"""
+    repo = _GetRepository()
+    tags = repo.git.tag('--list', '[0-9]*\.[0-9]*\.[0-9]*\-*', '--sort=-v:refname', '--merged').split('\n')
+    return next(iter(tags or ['']), '')
+
+
+def _ParseRawVersionString(raw):
+    """Break apart a raw version string into its various components,
+    and return those entries via a dictionary."""
+
+    components = { }    
+    rawComponents = raw.split("-")
+    
+    base = rawComponents[0]
+    patchRaw = rawComponents[1]
+    components['isFinal'] = ((patchRaw[-5:] == "final") or
+                             (patchRaw[-7:] == "release"))
+  
+    baseComponents = base.split(".")
+    components['major'] = int(baseComponents[0])
+    components['minor'] = int(baseComponents[1])
+    components['revision'] = int(baseComponents[2])
+  
+    patchComponents = patchRaw.split(".")
+    components['patch'] = int(patchComponents[0])
+    if (len(patchComponents) < 2):
+        components['patchType'] = "alpha"
+        components['patch'] = -1
+    else:
+        components['patchType'] = patchComponents[1]
+    
+    repo = _GetRepository()
+    components['hash'] = repo.git.rev_parse('--short=8', 'HEAD')  
+    return components
+
+
+def _CheckGitAvailable():
+    """Try the most basic of git commands, to see if there is
+       currently any access to a repository."""
+    try:
+        repo = _GetRepository()
+    except git.exc.InvalidGitRepositoryError:
+        return False
+    
+    return True
+
+
+def _GetRepository():
+    """Get a reference to the Git Repository.
+    Is there a cleaner option than searching from the current location?"""
+    # The syntax is different between library versions (particularly,
+    # those used by Centos 6 vs Centos 7).
+    try:
+        return git.Repo('.', search_parent_directories=True)
+    except TypeError:
+        return git.Repo('.')
+ 
+
+def _CheckDirtyRepository():
+    """Check to see if the repository is not in a cleanly committed state."""
+    repo = _GetRepository()
+    str = repo.git.status("--porcelain")
+    
+    return (len(str) > 0)
+
+
+def _ReadBackupVersionFile(target):
+    """There should be a file checked in with the latest version
+    information available; if git isn't available to provide
+    information, then use this file instead."""
+
+    with open(target, 'r') as fp:
+        line = fp.readline()
+
+    return line
+
+
+def _GetDateString():
+    """Returns formatted date string representing current UTC time"""
+    return datetime.utcnow().strftime("%Y%m%d%H%M")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--long", action="store_true", help="Output long format of version string")
+    args = parser.parse_args()
+
+    # default parameter to GetVersion functions
+    self_path, _ = os.path.split(os.path.realpath(__file__))
+    version_file = os.path.join(self_path, '../version.txt')
+
+    version = ""
+    if args.long:
+        version = GetLongVersion(version_file)
+    else:
+        version = GetVersion(version_file)
+
+    print version
+
+
+if __name__ == "__main__":
+    main()
