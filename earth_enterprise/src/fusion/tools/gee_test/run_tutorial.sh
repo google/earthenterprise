143e8b0aac50c259fe77e4aadc464aebac28efea
Colby Jeffries <cjeffries@t-sciences.com>
2018-10-31 13:12:03 -0500

    Update copyright changes to be correct.

--- earth_enterprise/src/fusion/tools/gee_test/run_tutorial.sh
+++ earth_enterprise/src/fusion/tools/gee_test/run_tutorial.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 #
-# Copyright 2018 Google Inc.
+# Copyright 2017 Google Inc.
+# Copyright 2018 Open GEE Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
