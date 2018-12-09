96f37f160fa1ea0f7d4f73346b018999948e799a
Doug Brantner <43447704+float13@users.noreply.github.com>
2018-12-09 12:40:03 -0500

    replace tabs with spaces

--- earth_enterprise/src/fusion/tools/migrate_fusion_hostname_unittest.py
+++ earth_enterprise/src/fusion/tools/migrate_fusion_hostname_unittest.py
@@ -9,47 +9,47 @@ import migrate_fusion_hostname as mfh
 
 class TestMigrateFusionHostname(unittest.TestCase):
 
-	"""Setup/Teardown"""
-	@classmethod
-	def setUpClass(cls):
-		"""Runs ONLY once at beginning."""
-		pass
+    """Setup/Teardown"""
+    @classmethod
+    def setUpClass(cls):
+        """Runs ONLY once at beginning."""
+        pass
 
-	@classmethod
-	def tearDownClass(cls):
-		"""Runs ONLY once at end."""
-		pass
+    @classmethod
+    def tearDownClass(cls):
+        """Runs ONLY once at end."""
+        pass
 
-	def setUp(self):
-		"""Runs once before every test."""
-		pass
+    def setUp(self):
+        """Runs once before every test."""
+        pass
 
-	def tearDown(self):
-		"""Runs once after every test."""
+    def tearDown(self):
+        """Runs once after every test."""
 
 
-	def testFullProgram(self):
-		# TODO big tests of full program as subprocess calls
-		# TODO test bad input, etc.
-		# *** run all of these ase DRYRUNS!
-		pass
+    def testFullProgram(self):
+        # TODO big tests of full program as subprocess calls
+        # TODO test bad input, etc.
+        # *** run all of these ase DRYRUNS!
+        pass
 
 
 # TODO split into smaller functions (1 per test?)
-	def testDaemonStartStop(self):
-		# Test Bad Input for daemon:
-		with self.assertRaises(OSError):
-			mfh.daemon_start_stop("/etc/init.d/fake_daemon_asdf", "start")
+    def testDaemonStartStop(self):
+        # Test Bad Input for daemon:
+        with self.assertRaises(OSError):
+            mfh.daemon_start_stop("/etc/init.d/fake_daemon_asdf", "start")
 
-		# Test Bad Input for command:
-		with self.assertRaises(ValueError):
-			mfh.daemon_start_stop("/etc/init.d/gefusion", None)
-			mfh.daemon_start_stop("/etc/init.d/gefusion", 1)
-			mfh.daemon_start_stop("/etc/init.d/gefusion", "")
-			mfh.daemon_start_stop("/etc/init.d/gefusion", "asdf")
+        # Test Bad Input for command:
+        with self.assertRaises(ValueError):
+            mfh.daemon_start_stop("/etc/init.d/gefusion", None)
+            mfh.daemon_start_stop("/etc/init.d/gefusion", 1)
+            mfh.daemon_start_stop("/etc/init.d/gefusion", "")
+            mfh.daemon_start_stop("/etc/init.d/gefusion", "asdf")
 
 
-			
+            
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
