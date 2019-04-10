a041e40c491e6a64ee4262ada8efda2d017e7fc1
Cloud User <centos@tstserver.vm>
2019-04-10 13:30:25 +0000

    Minor cleanup of SharedString.h. Added class comment.

--- earth_enterprise/src/common/SharedString.h
+++ earth_enterprise/src/common/SharedString.h
@@ -24,27 +24,37 @@
 #include <stdint.h>
 #include <assert.h>
 
-class SharedString/*: public std::string*/ {
+// The goal of this class is to keep in memory only one copy of the name of
+// a cached asset or asset version. Previously, each asset stored the full names
+// of all of its children, parents, listeners, and inputs, resulting in tens of
+// thousands of duplicates of each string in very large projects.
+//
+// With this class, only an integer identifier is stored in each instance (thus
+// drastically reducing the amount of memory needed in large projects), and
+// that identifier is used to retrieve the string value when it is requested.
+// Since the string-to-identifier ratio is 1:1, equality comparisons are also 
+// much faster as we only need to compare the identifiers.
+class SharedString {
 protected:
    class RefStorage {
       private:
-        std::unordered_map</*AssetDefs::AssetKey*/uint32_t, std::string> refFromKeyTable;
-        std::unordered_map<std::string, /*AssetDefs::AssetKey*/uint32_t> keyFromRefTable;
+        std::unordered_map<uint32_t, std::string> refFromKeyTable;
+        std::unordered_map<std::string, uint32_t> keyFromRefTable;
         static uint32_t nextID;
       public:
-        std::string RefFromKey(const /*AssetDefs::AssetKey*/uint32_t &key) {
+        std::string RefFromKey(const uint32_t &key) {
           auto refIter = refFromKeyTable.find(key);
           assert(refIter != refFromKeyTable.end());
           return refIter->second;
         }
-        /*AssetDefs::AssetKey*/uint32_t KeyFromRef(const std::string &ref) {
+        uint32_t KeyFromRef(const std::string &ref) {
           auto keyIter = keyFromRefTable.find(ref);
           if (keyIter != keyFromRefTable.end()) {
             return keyIter->second;
           }
           else {
             // We've never seen this ref before, so make a new key
-            /*AssetDefs::AssetKey*/uint32_t key = nextID++; // For the initial pass, key == ref
+            uint32_t key = nextID++; 
             refFromKeyTable.insert(std::pair<uint32_t, std::string>(key, ref));
             keyFromRefTable.insert(std::pair<std::string, uint32_t>(ref, key));
             return key;
