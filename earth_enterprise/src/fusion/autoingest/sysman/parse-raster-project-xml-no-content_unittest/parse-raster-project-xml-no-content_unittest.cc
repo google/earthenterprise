3b126ea3fc145a11edf6ef41c3717c2e2ec79bfc
Lee Savoie <lsavoie@t-sciences.com>
2019-05-22 13:22:43 -0700

    Fix compile errors

--- earth_enterprise/src/fusion/autoingest/sysman/parse-raster-project-xml-no-content_unittest/parse-raster-project-xml-no-content_unittest.cc
+++ earth_enterprise/src/fusion/autoingest/sysman/parse-raster-project-xml-no-content_unittest/parse-raster-project-xml-no-content_unittest.cc
@@ -78,7 +78,7 @@ class ParseRasterProjectXmlNoContent_Test :
 
         // Currently, this causes a segmentation fault:
         // Cause the reference to be resolved by loading the XML:
-        asset_ref_character_count += asset->GetRef().length();
+        asset_ref_character_count += asset->GetRef().toString().length();
     }
 
     virtual bool run_test_operation()
