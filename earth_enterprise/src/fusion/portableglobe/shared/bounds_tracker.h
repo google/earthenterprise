153ccb3700da7be9d125e2a726fddc63630fb97a
Jeremiah LaRocco <jlarocco@t-sciences.com>
2019-05-31 19:08:41 -0600

    1302: WIP - Code cleanup.
    Added and fixed documentation comments, and some small refactoring.

--- earth_enterprise/src/fusion/portableglobe/shared/bounds_tracker.h
+++ earth_enterprise/src/fusion/portableglobe/shared/bounds_tracker.h
@@ -27,65 +27,101 @@
 
 namespace fusion_portableglobe {
 
-
+/**
+   The boundary and zoom level information for a channel/layer.
+*/
 struct channel_info {
+  // Channel ID and type
+  uint16_t channel_id = UINT16_MAX;
+  PacketType type;
+
+  // Map tile bounding box
   uint32_t top = 0;
   uint32_t bottom = UINT32_MAX;
   uint32_t left = UINT32_MAX;
   uint32_t right = 0;
 
+  // Zoom levels where this channel is available
   uint32_t min_level = UINT32_MAX;
   uint32_t max_level = 0;
-  uint16_t channel = UINT16_MAX;
-  PacketType type;
-  channel_info() : top(0),
+
+  /**
+     Default constructor that creates an empty bounds, where
+     bottom > top, left > right, and min_level > max_level.
+  */
+  channel_info() : channel_id(UINT16_MAX),
+                   top(0),
                    bottom(UINT32_MAX),
                    left(UINT32_MAX),
                    right(0),
                    min_level(UINT32_MAX),
-                   max_level(0),
-                   channel(UINT16_MAX)
+                   max_level(0)
   {};
-  channel_info(uint32_t row,
+
+  /**
+     Create a channel_info with the given channel_id and type, bounding the
+     given row, column, and level.
+     @param channel
+     @param type
+     @param row
+     @param column
+     @param level
+  */
+  channel_info(uint16_t channel_id,
+               PacketType type,
+               uint32_t row,
                uint32_t column,
-               uint32_t level,
-               uint16_t channel,
-               PacketType type) :
+               uint32_t level) :
+    channel_id(channel_id),
+    type(type),
     top(row), bottom(row),
     left(column), right(column),
-    min_level(level), max_level(level),
-    channel(channel),
-    type(type)
+    min_level(level), max_level(level)
   {}
 };
 
 /**
-   A class to keep track of imagery, terrain, and vector boundaries while
-   building maps and globes.
+   A class for tracking the map tile boundaries and zoom levels of a portable file
+   as it's being built. Once the globe is finished, the information is saved to
+   earth/metadata.json and included in the final *.glb or *.glm file.
+
+   Only the tile boundaries at max_level are stored by the tracker.
  */
+
 class BoundsTracker {
 public:
+
+  /// Default constructor
   BoundsTracker();
 
   /**
-     Update min_level and max_level in/out parameters to incorporate qtpath.
-     If update is true, also update the top, bottom, left, and right boundaries.
-     @param qtpath The quadtree path to incorporate
-     @param type   The type of node being added.
+     Update the channel_info for the specified channel to be of the given type and
+     include qtpath in its bounds.
+     @param channel ID of the channel to be updated.
+     @param type    The packet type of the channel.
+     @param qtpath  The quadtree address to update.
   */
-  void update(const std::string& qtpath, PacketType type, uint16_t channel);
+  void update(uint16_t channel, PacketType type, const std::string& qtpath);
 
   /**
-     Write current boundary information to a JSON file.
-     @param filename The name of the file to create.
+     Write current boundary information to the specified JSON file.
+     @param filename
   */
-  void write_json_file(const std::string& filename);
+  void write_json_file(const std::string& filename) const;
 
-public:
+  /**
+     Return a const reference to the specified channel_info object.
+     Throws a khSimpleException if the channel isn't found.
+     @param channel_id
+  */
+  const channel_info& channel(uint16_t channel_id) const;
+
+
+private:
+
   // Minimum and maximum zoom level and channel for imagery, terrain, and vector data
   std::map<uint16_t, channel_info> channels;
 
-private:
   DISALLOW_COPY_AND_ASSIGN(BoundsTracker);
 };
 
