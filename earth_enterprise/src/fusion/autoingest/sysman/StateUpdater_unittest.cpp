dd10465f57f2c48c699f3cf37eab7d73fd4c6a25
Lee Savoie <lsavoie@t-sciences.com>
2019-07-18 09:34:03 -0700

    First state updater unit tests

--- earth_enterprise/src/fusion/autoingest/sysman/StateUpdater_unittest.cpp
+++ earth_enterprise/src/fusion/autoingest/sysman/StateUpdater_unittest.cpp
@@ -18,25 +18,42 @@
 #include "gee_version.h"
 #include "StateUpdater.h"
 
+#include <assert.h>
 #include <gtest/gtest.h>
+#include <map>
 #include <string>
 
 using namespace std;
 
 class MockVersion : public AssetVersionImpl {
-  private:
-    bool needComputeState;
   public:
-    MockVersion(bool computeState) : needComputeState(computeState) {}
+    bool stateSet;
+
+    MockVersion() : stateSet(false) {
+      type = AssetDefs::Imagery;
+      state = AssetDefs::New;
+    }
+    MockVersion(const AssetKey & ref) : MockVersion() {
+      // All refs must end with "?version=X" or the calls to AssetVersionRef::Bind
+      // will try to load assets from disk. For simplicity, we force everything to
+      // be version 1.
+      string suffix = "?version=1";
+      string refStr = ref.toString();
+      assert(name.empty() || equal(suffix.rbegin(), suffix.rend(), refStr.rbegin()));
+      name = ref;
+    }
+    MockVersion(const MockVersion & that) : MockVersion(that.name) {}
     void DependentChildren(vector<SharedString> & dependents) const {}
-    bool NeedComputeState() const { return needComputeState; }
+    bool NeedComputeState() const { return true; }
     AssetDefs::State CalcStateByInputsAndChildren(
         AssetDefs::State stateByInputs,
         AssetDefs::State stateByChildren,
         bool blockersAreOffline,
         uint32 numWaitingFor) const
     { return AssetDefs::New; }
-    void SetMyStateOnly(AssetDefs::State newState, bool sendNotifications) {}
+    void SetMyStateOnly(AssetDefs::State newState, bool sendNotifications) {
+      stateSet = true;
+    }
     
     // Not used - only included to make MockVersion non-virtual
     string PluginName(void) const { return string(); }
@@ -44,28 +61,68 @@ class MockVersion : public AssetVersionImpl {
     string GetOutputFilename(uint) const { return string(); }
 };
 
+using VersionMap = map<AssetKey, shared_ptr<MockVersion>>;
+
 class MockStorageManager : public StorageManagerInterface<AssetVersionImpl> {
   private:
-    AssetPointerType<AssetVersionImpl> Get() {
-      auto mockPtr = make_shared<MockVersion>(false);
-      return dynamic_pointer_cast<AssetVersionImpl>(mockPtr);
+    VersionMap versions;
+    PointerType GetFromMap(const AssetKey & ref) {
+      auto versionIter = versions.find(ref);
+      if (versionIter != versions.end()) {
+        auto version = dynamic_pointer_cast<AssetVersionImpl>(versionIter->second);
+        assert(version);
+        return version;
+      }
+      assert(false);
+      return nullptr;
     }
   public:
-    virtual AssetHandle<const AssetVersionImpl> Get(const AssetKey &) { return AssetHandle<const AssetVersionImpl>(Get(), nullptr); }
-    virtual AssetHandle<AssetVersionImpl> GetMutable(const AssetKey &) { return AssetHandle<AssetVersionImpl>(Get(), nullptr); }
+    void AddVersion(const AssetKey & key, const MockVersion & v) {
+      versions[key] = make_shared<MockVersion>(v);
+    }
+    virtual AssetHandle<const AssetVersionImpl> Get(const AssetKey &ref) {
+      return AssetHandle<const AssetVersionImpl>(GetFromMap(ref), nullptr);
+    }
+    virtual AssetHandle<AssetVersionImpl> GetMutable(const AssetKey &ref) {
+      return AssetHandle<AssetVersionImpl>(GetFromMap(ref), nullptr);
+    }
 };
 
 class StateUpdaterTest : public testing::Test {
   protected:
-   MockStorageManager storageManager;
+   MockStorageManager sm;
    StateUpdater updater;
   public:
-   StateUpdaterTest()
-     : storageManager(),
-       updater(&storageManager) {}
+   StateUpdaterTest() : sm(), updater(&sm) {}
 };
 
-TEST_F(StateUpdaterTest, NullTest) {
+MockVersion MakeVersion(const AssetKey & ref) {
+  return MockVersion(ref);
+}
+
+void SetVersions(MockStorageManager & sm, vector<MockVersion> versions) {
+  for (auto & version: versions) {
+    sm.AddVersion(version.name, version);
+  }
+}
+
+const MockVersion * GetVersion(MockStorageManager & sm, const AssetKey & key) {
+  auto version = sm.Get(key);
+  return dynamic_cast<const MockVersion*>(version.operator->());
+}
+
+TEST_F(StateUpdaterTest, SetStateSingleVersion) {
+  AssetKey ref1 = "test1?version=1";
+  AssetKey ref2 = "test2?version=1";
+  AssetKey ref3 = "test3?version=1";
+  SetVersions(sm, {MakeVersion(ref1), MakeVersion(ref2), MakeVersion(ref3)});
+  updater.SetStateForRefAndDependents(ref1, AssetDefs::Bad, [](AssetDefs::State) { return true; });
+  ASSERT_TRUE(GetVersion(sm, ref1)->stateSet);
+  ASSERT_FALSE(GetVersion(sm, ref2)->stateSet);
+  ASSERT_FALSE(GetVersion(sm, ref3)->stateSet);
+  updater.SetStateForRefAndDependents(ref2, AssetDefs::Bad, [](AssetDefs::State) { return false; });
+  ASSERT_FALSE(GetVersion(sm, ref2)->stateSet);
+  ASSERT_FALSE(GetVersion(sm, ref3)->stateSet);
 }
 
 int main(int argc, char **argv) {
