2bbf0938cfc0c5045062f964cfe86f9b39dfc36d
Lee Savoie <lsavoie@t-sciences.com>
2019-07-22 09:23:06 -0700

    Conversion tests

--- earth_enterprise/src/fusion/autoingest/StorageManagerAssetHandle_unittest.cpp
+++ earth_enterprise/src/fusion/autoingest/StorageManagerAssetHandle_unittest.cpp
@@ -23,9 +23,13 @@ using namespace std;
 
 class TestItem {
   public:
-    TestItem() : type(AssetDefs::Invalid) {}
+    TestItem() : type(AssetDefs::Imagery) {}
+    virtual ~TestItem() = default;
     AssetDefs::Type type;
 };
+
+class DerivedItem1 : public TestItem {};
+class DerivedItem2 : public TestItem {};
 
 TEST(AssetHandleTest, EmptyHandle) {
   AssetHandle<TestItem> empty;
@@ -35,7 +39,6 @@ TEST(AssetHandleTest, EmptyHandle) {
 
 TEST(AssetHandleTest, BasicHandle) {
   auto item = make_shared<TestItem>();
-  item->type = AssetDefs::Imagery;
   AssetHandle<TestItem> handle(item, nullptr);
   ASSERT_TRUE(handle.operator bool());
   ASSERT_EQ(handle.operator->(), &*item);
@@ -43,8 +46,40 @@ TEST(AssetHandleTest, BasicHandle) {
 
 TEST(AssetHandleTest, InvalidItem) {
   auto item = make_shared<TestItem>();
+  item->type = AssetDefs::Invalid;
   AssetHandle<TestItem> handle(item, nullptr);
   ASSERT_FALSE(handle.operator bool());
+}
+
+// TODO: onfinalize
+
+TEST(AssetHandleTest, DownCast) {
+  auto item = make_shared<DerivedItem1>();
+  AssetHandle<DerivedItem1> derived(item, nullptr);
+  AssetHandle<TestItem> test = derived;
+  ASSERT_TRUE(test.operator bool());
+}
+
+TEST(AssetHandleTest, UpCast) {
+  auto item = make_shared<DerivedItem1>();
+  AssetHandle<TestItem> test(item, nullptr);
+  ASSERT_TRUE(test.operator bool());
+  AssetHandle<DerivedItem1> derived = test;
+  ASSERT_TRUE(derived.operator bool());
+}
+
+TEST(AssetHandleTest, IncompatibleConversion1) {
+  auto item = make_shared<DerivedItem1>();
+  AssetHandle<DerivedItem1> derived1(item, nullptr);
+  ASSERT_TRUE(derived1.operator bool());
+  ASSERT_DEATH({AssetHandle<DerivedItem2> derived2 = derived1;}, ".*");
+}
+
+TEST(AssetHandleTest, IncompatibleConversion2) {
+  auto item = make_shared<DerivedItem1>();
+  AssetHandle<TestItem> derived1(item, nullptr);
+  ASSERT_TRUE(derived1.operator bool());
+  ASSERT_DEATH({AssetHandle<DerivedItem2> derived2 = derived1;}, ".*");
 }
 
 int main(int argc, char **argv) {
