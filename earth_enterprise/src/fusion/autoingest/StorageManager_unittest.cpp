307a33bc9a549df75575255cb43372d60c9328ac
Lee Savoie <lsavoie@t-sciences.com>
2019-08-09 11:22:46 -0700

    Revert "Move load and save code into the storage manager (#1390)"
    This reverts commit f407c39a06280ec8ef19647a4d8f80b42024cd85.

--- earth_enterprise/src/fusion/autoingest/StorageManager_unittest.cpp
+++ earth_enterprise/src/fusion/autoingest/StorageManager_unittest.cpp
@@ -27,15 +27,8 @@ using namespace std;
 
 const size_t CACHE_SIZE = 5;
 
-class TestItemStorage {
-  public:
-    TestItemStorage() {}
-};
-
-class TestItem : public StorageManaged, public TestItemStorage {
+class TestItem : public StorageManaged {
  public:
-  using Base = TestItemStorage;
-
   static int nextValue;
   static string fileName;
   static bool isValidRef;
@@ -43,17 +36,15 @@ class TestItem : public StorageManaged, public TestItemStorage {
   const int val;
   AssetDefs::Type type;
   string savename;
-  string loadname;
   bool saveSucceeds;
   TestItem() : val(nextValue++), type(AssetDefs::Imagery), saveSucceeds(true) {}
   const string XMLFilename() {
     return TestItem::Filename(SharedString());
   }
-
-  std::string GetName() const {
-    return "TestItem";
+  virtual bool Save(const string &filename) {
+    savename = filename;
+    return saveSucceeds;
   }
-
   // determine amount of memory used by TestItem
   uint64 GetSize() {
     return (GetObjectSize(val)
@@ -71,21 +62,10 @@ class TestItem : public StorageManaged, public TestItemStorage {
   static bool ValidRef(const SharedString & ref) {
     return isValidRef;
   }
+  static typename StorageManager<TestItem>::PointerType Load(const string &) {
+    return std::make_shared<TestItem>();
+  }
 };
-
-class TestSerializer : public AssetSerializerInterface<TestItem> {
-  public:
-    virtual AssetPointerType<TestItem> Load(const std::string & filename) {
-      auto asset = std::make_shared<TestItem>();
-      asset->loadname = filename;
-      return asset;
-    }
-    virtual bool Save(AssetPointerType<TestItem> asset, std::string filename) {
-      asset->savename = filename;
-      return asset->saveSucceeds;
-    }
-};
-
 int TestItem::nextValue;
 string TestItem::fileName;
 bool TestItem::isValidRef;
@@ -95,6 +75,9 @@ using PointerType = AssetPointerType<TestItem>;
 
 class TestHandle : public AssetHandleInterface<TestItem> {
   public:
+    virtual PointerType Load(const string &) const {
+      return PointerType(std::make_shared<TestItem>());
+    }
     virtual bool Valid(const PointerType &) const { return true; }
     TestHandle(const AssetKey & name) : name(name) {}
     TestHandle() = default;
@@ -117,7 +100,7 @@ class StorageManagerTest : public testing::Test {
  protected:
   StorageManager<TestItem> storageManager;
  public:
-  StorageManagerTest() : storageManager(CACHE_SIZE, false, 0, "test", unique_ptr<TestSerializer>(new TestSerializer())) {
+  StorageManagerTest() : storageManager(CACHE_SIZE, false, 0, "test") {
     // Reset the static variables in TestItem
     TestItem::nextValue = 1;
     TestItem::fileName = "/dev/null"; // A file that exists
@@ -141,7 +124,6 @@ TEST_F(StorageManagerTest, AddAndRetrieveLegacy) {
   ASSERT_EQ(first.handle->val, startValue) << "First item has unexpected value";
   ASSERT_EQ(second.handle->val, startValue + 1) << "Second item has unexpected value";
   ASSERT_EQ(third.handle->val, startValue + 2) << "Third item has unexpected value";
-  ASSERT_EQ(first.handle->loadname, "first") << "Asset loaded from wrong file";
   
   // Retrieve one of the items
   TestHandle second2 = Get<TestHandle>(storageManager, "second", false, true, false);
@@ -329,7 +311,10 @@ TEST_F(StorageManagerTest, SaveDirtyLegacy) {
   ASSERT_EQ(trans.NumNew(), 2) << "Wrong number of new items in file transaction";
   ASSERT_EQ(trans.NumDeleted(), 0) << "Wrong number of deleted items in file transaction";
   ASSERT_EQ(handles[0].handle->savename, string()) << "Non-dirty files should not be saved";
-  ASSERT_EQ(handles[4].handle->savename, "/dev/null.new") << "Asset saved to wrong file";
+  ASSERT_NE(handles[4].handle->savename, string()) << "Dirty files should be saved";
+  string savename = handles[4].handle->savename;
+  string ext = ".new";
+  ASSERT_TRUE(equal(ext.rbegin(), ext.rend(), savename.rbegin())) << "Saved file name must end in .new";
 }
 
 TEST_F(StorageManagerTest, SaveDirtyToVectorLegacy) {
@@ -372,8 +357,7 @@ TEST_F(StorageManagerTest, AddAndRetrieve) {
   ASSERT_EQ(first->val, startValue) << "First item has unexpected value";
   ASSERT_EQ(second->val, startValue + 1) << "Second item has unexpected value";
   ASSERT_EQ(third->val, startValue + 2) << "Third item has unexpected value";
-  ASSERT_EQ(first->loadname, "first") << "Asset loaded from wrong file";
-
+  
   // Retrieve one of the items
   auto second2 = storageManager.Get("second");
   ASSERT_EQ(storageManager.CacheSize(), 3) << "Storage manager has wrong number of items in cache";
@@ -538,7 +522,10 @@ TEST_F(StorageManagerTest, SaveDirty) {
   ASSERT_EQ(trans.NumNew(), 2) << "Wrong number of new items in file transaction";
   ASSERT_EQ(trans.NumDeleted(), 0) << "Wrong number of deleted items in file transaction";
   ASSERT_EQ(handles[0]->savename, string()) << "Non-dirty files should not be saved";
-  ASSERT_EQ(mutHandles[1]->savename, "/dev/null.new") << "Asset saved to wrong file";
+  ASSERT_NE(mutHandles[1]->savename, string()) << "Dirty files should be saved";
+  string savename = mutHandles[1]->savename;
+  string ext = ".new";
+  ASSERT_TRUE(equal(ext.rbegin(), ext.rend(), savename.rbegin())) << "Saved file name must end in .new";
 }
 
 TEST_F(StorageManagerTest, SaveDirtyToVector) {
