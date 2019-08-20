f2ff5ba9d250b8d34c5b48429ba5f632cb2040b1
tst-nrichman <49651366+tst-nrichman@users.noreply.github.com>
2019-08-20 11:47:18 -0500

    Skip Internal Certificate Verification (#1406)
    * Skip Internal Certificate Verification
    
    Fixes #1404. Allows Python to bypass the internal certificate verification by default. Extra security configurations are also added to allow users to redirect the host and verify against specific certificates.
    
    * Update copyright version of new wms config file
    
    Updates the copyright version of a newly created file.
    
    * Remove unused certificate locations
    
    Removes variables used to store certificate locations during development.
    
    * Remove unused variables and update copyright information
    
    Removes more unused certificate paths. Also updates the copyright for each file to include Open Gee Contributor copyright dates.
    
    * Default urlopen variable to None
    
    Sets the default of urlopen to None. This is so if the urlopen fails, the file does not fail its close check.
    
    * Fix Google copyright
    
    Removes the updated copyright year.

---
+++ earth_enterprise/src/server/wsgi/wms/ogc/common/wms_connection.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Open GEE Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Handle connections to WMS API"""
+
+import ssl
+import urllib2
+import urlparse
+import logging
+import configs
+
+CONFIG_FILE = "/opt/google/gehttpd/wsgi-bin/wms/ogc/wms.cfg"
+CONFIGS = configs.Configs(CONFIG_FILE)
+
+logger = logging.getLogger("wms_maps")
+
+def HandleConnection(url):
+  logger.debug("Opening url: [%s]", url)
+
+  if CONFIGS.GetStr("DATABASE_HOST") != "":
+    url = CONFIGS.GetStr("DATABASE_HOST") + urlparse.urlsplit(url)[2:]
+
+  fp = None
+  try:
+    # Set the context based on cert requirements
+    if CONFIGS.GetBool("VALIDATE_CERTIFICATE"):
+      cert_file = CONFIGS.GetStr("CERTIFICATE_CHAIN_PATH")
+      key_file = CONFIGS.GetStr("CERTIFICATE_KEY_PATH")
+      context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+      context.load_cert_chain(cert_file, keyfile=key_file)
+    else:
+      context = ssl.create_default_context()
+      context.check_hostname = False
+      context.verify_mode = ssl.CERT_NONE
+
+    fp = urllib2.urlopen(url, context=context)
+    response = fp.read()
+  except urllib2.HTTPError, e:
+    logger.warning("Server definitions didn't return any results %s.", e)
+    return {}
+  finally:
+    if fp:
+      fp.close()
+
+  return response
