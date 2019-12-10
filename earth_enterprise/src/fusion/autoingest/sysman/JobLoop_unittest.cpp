ffd0dfcf34008f3184b85c3f76f1b7e0c2d89a68
Lee Savoie <lsavoie@t-sciences.com>
2019-12-10 08:07:21 -0700

    Link all necessary files with unit tests

--- earth_enterprise/src/fusion/autoingest/sysman/JobLoop_unittest.cpp
+++ earth_enterprise/src/fusion/autoingest/sysman/JobLoop_unittest.cpp
@@ -16,6 +16,12 @@
 
 #include <gtest/gtest.h>
 
+#include "khResourceProvider.h"
+
+TEST(JobLoopTest, CreateResourceProvider) {
+  khResourceProvider resProv;
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc,argv);
   return RUN_ALL_TESTS();
