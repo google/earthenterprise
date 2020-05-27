c211b0fbd2b79924d659d36cb03c2dbbf621118d
David Sugar <dsugar@t-sciences.com>
2020-05-27 15:30:58 +0000

    mttypes fix

--- earth_enterprise/src/common/mttypes/DrainableQueue.h
+++ earth_enterprise/src/common/mttypes/DrainableQueue.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2017 Google Inc.
+ * Copyright 2020 The Open GEE Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -63,11 +64,11 @@ class BatchingDrainableQueuePusher : public BatchingQueuePusher<T> {
  public:
   typedef typename Queue<T>::Item       Item;
 
-  BatchingDrainableQueuePusher(uint32 batch_size, DrainableQueue<T> &queue) :
+  BatchingDrainableQueuePusher(std::uint32_t batch_size, DrainableQueue<T> &queue) :
       BatchingQueuePusher<T>(batch_size, queue)
   {
   }
-  
+
   void PushWithDrain(const T &t) {
     BatchingQueuePusher<T>::AddToBatch(Item(t, Item::DrainFlag));
   }
@@ -88,7 +89,7 @@ class BatchingDrainableQueuePusher : public BatchingQueuePusher<T> {
 template <class T>
 class BatchingDrainableQueuePuller : public BatchingQueuePuller<T> {
  public:
-  BatchingDrainableQueuePuller(uint32 batch_size, DrainableQueue<T> &queue) :
+  BatchingDrainableQueuePuller(std::uint32_t batch_size, DrainableQueue<T> &queue) :
       BatchingQueuePuller<T>(batch_size, queue)
   {
     this->want_backref_ = true;
