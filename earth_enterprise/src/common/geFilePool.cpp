351f2500b3f7e74fc0a68459a3f6f4d5639989dd
tst-jkent <jkent@t-sciences.com>
2020-05-28 20:51:44 +0000

    Add condition for test dir to getAccessor.

--- earth_enterprise/src/common/geFilePool.cpp
+++ earth_enterprise/src/common/geFilePool.cpp
@@ -49,7 +49,7 @@ bool POSIXFileAccessor::PwriteAll(const void* buffer, size_t size, off64_t offse
 namespace FileAccessorFactory {
   static std::unique_ptr<AbstractFileAccessor> getAccessor(const std::string &fname, int flags, mode_t createMask) {
     notify(NFY_WARN, "Filename: %s", fname.c_str());
-    if (fname.rfind("/gevol/src/", 0) == 0) {
+    if (fname.rfind("/gevol", 0) == 0 || fname.rfind("/tmp/tests", 0) == 0) {
       std::unique_ptr<AbstractFileAccessor> pFA = std::unique_ptr<POSIXFileAccessor>{new POSIXFileAccessor{}};
       pFA->setFD(pFA->Open(fname, flags, createMask));
       return pFA;
