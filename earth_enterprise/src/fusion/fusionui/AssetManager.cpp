f42111d2903bc71bb4b9eeff7f561cf328ad870a
David Sugar <32524063+tst-dsugar@users.noreply.github.com>
2020-06-17 09:30:25 -0400

    Update AssetManager.cpp
    copyright cleanup

--- earth_enterprise/src/fusion/fusionui/AssetManager.cpp
+++ earth_enterprise/src/fusion/fusionui/AssetManager.cpp
@@ -1,6 +1,5 @@
 // Copyright 2020 the Open GEE Contributors.
 // Copyright 2017 Google Inc.
-// Copyright 2020 OpenGEE Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
