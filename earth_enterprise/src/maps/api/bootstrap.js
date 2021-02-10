9a2e3a627e3a6fc2ac1cabc5c048e035db311c0a
cwgrant <cwgrant@users.noreply.github.com>
2021-02-10 13:16:54 -0500

    Fix issues with external sites loading google maps (#1880)

--- earth_enterprise/src/maps/api/bootstrap.js
+++ earth_enterprise/src/maps/api/bootstrap.js
@@ -1,11 +1,11 @@
 //  Copyright 2017 Google Inc.
-// 
+//
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
 //  You may obtain a copy of the License at
-// 
+//
 //       http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,21 +22,28 @@ google.maps = google.maps || {};
     document.write('<script src="' + src +
                    '" type="text/javascript"></script>');
   }
-  
+
   GEE_API_PATH = '/maps/api/js';
   FUSION_API_PATH = '/maps/api';
 
   google.maps.Load = function(apiLoad) {
-    /* http://mt0.google.com/vt */    apiLoad([,[,,,],['en-US',,,,,,GEE_BASE_URL + '/maps/api/icons/',],[GEE_BASE_URL + GEE_API_PATH,'internal'],[0],0.0,,,,,], loadScriptTime);
+    /* http://mt0.google.com/vt */    apiLoad([,[,,,],['en-US',,,,,,GEE_BASE_URL + '/maps/api/icons/',
+                                                        GEE_BASE_URL /* GE metrics */,
+                                                        ,
+                                                        GEE_BASE_URL /* GE metrics */],
+                                                        [GEE_BASE_URL + GEE_API_PATH,'internal'],[0],0.0,,,,,], loadScriptTime);
   };
 
   var loadScriptTime = (new Date).getTime();
 
-  getScript(GEE_BASE_URL + '/js/jquery.js');
+  // Only load jQuery if it isn't loaded already.
+  if (!window.jQuery) {
+    getScript(GEE_BASE_URL + '/js/jquery.js');
+  }
+
   getScript(GEE_BASE_URL + GEE_API_PATH + '/main.js');
   getScript(GEE_BASE_URL + GEE_API_PATH + '/../fusion_map_obj_v3.js');
 
   getScript(GEE_BASE_URL + FUSION_API_PATH + '/fusion_extended_map.js');
   getScript(GEE_BASE_URL + FUSION_API_PATH + '/fusion_utils.js');
 })();
-
