2b686c62c5363f622e357840a4d491d8c955eb96
Colby Jeffries <cjeffries@t-sciences.com>
2021-03-05 14:13:49 -0600

    Test encoding fix.

--- earth_enterprise/src/server/wsgi/serve/push/stream/stream_push_app.py
+++ earth_enterprise/src/server/wsgi/serve/push/stream/stream_push_app.py
@@ -84,7 +84,7 @@ class StreamPushApp(object):
                              constants.STATUS_FAILURE)
     try:
       start_response(StreamPushApp.STATUS_OK, StreamPushApp.RESPONSE_HEADERS)
-      return response.body
+      return response.body.encode('ascii')
     except Exception:
       exc_info = sys.exc_info()
       start_response(StreamPushApp.STATUS_ERROR,
