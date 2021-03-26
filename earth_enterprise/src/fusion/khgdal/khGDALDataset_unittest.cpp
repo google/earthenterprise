36b5eb9ab193f3c1939a4cf66db8357fd10392e5
Lee Savoie <lsavoie@t-sciences.com>
2021-03-26 15:03:10 -0700

    Start on GDAL Dataset unit tests

---
+++ earth_enterprise/src/fusion/khgdal/khGDALDataset_unittest.cpp
@@ -0,0 +1,87 @@
+// Copyright 2021 The Open GEE Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include "khgdal/khGDALDataset.h"
+
+TEST(GDALDatasetTest, FlatLowRes) {
+  khGDALDataset ds("fusion/testdata/lowres.tiff");
+  ASSERT_EQ(6, ds.normalizedTopLevel());
+}
+
+TEST(GDALDatasetTest, FlatMedRes) {
+  khGDALDataset ds("fusion/testdata/medres.tiff");
+  ASSERT_EQ(14, ds.normalizedTopLevel());
+}
+
+TEST(GDALDatasetTest, FlatHighRes) {
+  khGDALDataset ds("fusion/testdata/highres.tiff");
+  ASSERT_EQ(31, ds.normalizedTopLevel());
+}
+
+// This verifies that the resolution tops out at the maximum zoom level
+TEST(GDALDatasetTest, FlatTooHighRes) {
+  khGDALDataset ds("fusion/testdata/toohighres.tiff");
+  ASSERT_EQ(31, ds.normalizedTopLevel());
+}
+
+TEST(GDALDatasetTest, MercLowRes) {
+  khGDALDataset ds("fusion/testdata/lowres.tiff",
+      std::string(),
+      khExtents<double>(),
+      khTilespace::MERCATOR_PROJECTION);
+  ASSERT_EQ(4, ds.normalizedTopLevel());
+}
+
+TEST(GDALDatasetTest, MercMedRes) {
+  khGDALDataset ds("fusion/testdata/medres.tiff",
+      std::string(),
+      khExtents<double>(),
+      khTilespace::MERCATOR_PROJECTION);
+  ASSERT_EQ(14, ds.normalizedTopLevel());
+}
+
+TEST(GDALDatasetTest, MercHighRes) {
+  khGDALDataset ds("fusion/testdata/highres.tiff",
+      std::string(),
+      khExtents<double>(),
+      khTilespace::MERCATOR_PROJECTION);
+  ASSERT_EQ(31, ds.normalizedTopLevel());
+}
+
+// This verifies that the resolution tops out at the maximum zoom level
+TEST(GDALDatasetTest, MercTooHighRes) {
+  khGDALDataset ds("fusion/testdata/toohighres.tiff",
+      std::string(),
+      khExtents<double>(),
+      khTilespace::MERCATOR_PROJECTION);
+  ASSERT_EQ(31, ds.normalizedTopLevel());
+}
+
+// This tests a case where the level is different between flat and mercator
+TEST(GDALDatasetTest, FlatVsMerc) {
+  khGDALDataset flat("fusion/testdata/flatvsmerc.tiff");
+  khGDALDataset merc("fusion/testdata/flatvsmerc.tiff",
+      std::string(),
+      khExtents<double>(),
+      khTilespace::MERCATOR_PROJECTION);
+  ASSERT_EQ(6, flat.normalizedTopLevel());
+  ASSERT_EQ(3, merc.normalizedTopLevel());
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
